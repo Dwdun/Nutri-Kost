070c56a9416e4422214278c83857f85cd4e8941e
Muhammad Faqih Shiam <faqihshiam23@gmail.com>
2026-05-04 05:03:34 +0700

    fix bug db ga load data

--- check_db.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-import os
-
-db_path = r'c:\Users\Faqih sh\OneDrive\Dokumen\NutriKos\Nutri-Kost\bima_scrapper\nutrikost.db'
-conn = sqlite3.connect(db_path)
-cur = conn.cursor()
-cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
-print('bima_scrapper/nutrikost.db Tables:', cur.fetchall())
